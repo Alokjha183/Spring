7afc339ddfe8bc5c168b1f3dee3ede89ae3d5cb3
Alokjha183 <alokraj.raj1@gmail.com>
2023-07-15 16:10:59 +0530

    First Commit

---
+++ src/main/java/in/alok/AppControoller.java
@@ -0,0 +1,13 @@
+package in.alok;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppControoller {
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello World";
+	}
+
+}
